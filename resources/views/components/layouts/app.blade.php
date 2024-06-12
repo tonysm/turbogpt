e61069f5fe4c4dcacedfc78892ebc43244be3f9c
Tony Messias <tonysm@hey.com>
2024-06-12 10:48:42 -0300

    WIP styles

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+@props(['title'])
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    {{ $slot }}
+</body>
+
+</html>
